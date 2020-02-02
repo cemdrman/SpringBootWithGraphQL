b26798f07bddef3366791d7f56ac3c9fddcefec3
Cem Dırman <cemdrman@gmail.com>
2020-02-02 16:50:14 +0300

    init

---
+++ src/main/java/com/bilisimio/GraphQlapiTrainingApplication.java
@@ -0,0 +1,13 @@
+package com.bilisimio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GraphQlapiTrainingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GraphQlapiTrainingApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/bilisimio/HelloQueryResolver.java
@@ -0,0 +1,14 @@
+package com.bilisimio;
+
+import org.springframework.stereotype.Service;
+
+import com.coxautodev.graphql.tools.GraphQLQueryResolver;
+
+@Service
+public class HelloQueryResolver implements GraphQLQueryResolver{
+	
+	public String hello() {
+        return "Hello GraphQL";
+    }
+
+}
