bd804170d89129ca08d48c403c60d711b80d1aeb
Cem Dırman <cemdrman@gmail.com>
2020-05-14 22:25:36 +0300

    service impl layer

--- src/main/java/com/bilisimio/service/BloggerService.java
+++ src/main/java/com/bilisimio/service/BloggerService.java
@@ -1,30 +1,11 @@
 package com.bilisimio.service;
 
-import java.util.ArrayList;
 import java.util.List;
-
-import org.springframework.stereotype.Service;
 
 import com.bilisimio.entity.Blogger;
 
-@Service
-public class BloggerService {
+public interface BloggerService {
 
-	private static List<Blogger> bloggerList = new ArrayList<>();
-
-	static {
-		bloggerList.add(new Blogger(1L, "Alper", "AKALIN", "admin@bilisim.io", "password"));
-		bloggerList.add(new Blogger(2L, "Kubilay", "KULAOĞLU", "admin@bilisim.io", "password"));
-		bloggerList.add(new Blogger(3L, "Ömer", "KOÇBİL", "admin@bilisim.io", "password"));
-		bloggerList.add(new Blogger(4L, "Barış", "UYAR", "admin@bilisim.io", "password"));
-		bloggerList.add(new Blogger(5L, "Burak", "KARADAĞ", "admin@bilisim.io", "password"));
-		bloggerList.add(new Blogger(6L, "Tahir", "KARDAK", "admin@bilisim.io", "password"));
-		bloggerList.add(new Blogger(7L, "Volkan", "ÖZDEMİR", "admin@bilisim.io", "password"));
-		bloggerList.add(new Blogger(8L, "Cem", "DIRMAN", "admin@bilisim.io", "password"));
-	}
-
-	public List<Blogger> getBloggerList() {
-		return bloggerList;
-	}	
-
+	List<Blogger> getBloggerList();
+	
 }
